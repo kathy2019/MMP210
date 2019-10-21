825fd2eb77796d0f6cf7a7ac84b144aa2163abfa
Katherine Tejeda <ktejeda@crotonaihs.org>
2019-10-21 18:24:18 -0400

    updates

--- Meme/sketch.js
+++ Meme/sketch.js
@@ -16,8 +16,8 @@ function draw (){
 
 	
 	background ("white");
-	image(puppy, 150, 50, width, height);
-	textSize(25);
+	image(puppy, 100, 50, width, height);
+	textSize(45);
 	fill("white")
 	textAlign(CENTER);
 	text("When you are relaxed", 500, 100);
